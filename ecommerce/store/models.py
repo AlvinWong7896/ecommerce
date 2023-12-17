b3e4afd4265b83b3af5d5bc007bf614a8700aced
CK Wong <alvinwong7896@gmail.com>
2023-12-17 09:59:37 +0800

    completion of Part 2- Data structure

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -34,22 +34,11 @@ class Order(models.Model):
         Customer, on_delete=models.SET_NULL, null=True, blank=True
     )
     date_ordered = models.DateTimeField(auto_now_add=True)
-    complete = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
-        shipping = False
-
-        return shipping
-
-    @property
-    def shipping(self):
-        shipping = False
-        orderitems = self.orderitem_set.all()
-        for i in orderitems:
-            if i.product.digital == False:
-                shipping = True
-        return shipping
+        return str(self.id)
 
     @property
     def get_cart_total(self):
@@ -65,8 +54,10 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
@@ -77,12 +68,14 @@ class OrderItem(models.Model):
 
 
 class ShippingAddress(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
-    address = models.CharField(max_length=200, null=False)
-    city = models.CharField(max_length=200, null=False)
-    state = models.CharField(max_length=200, null=False)
-    zipcode = models.CharField(max_length=200, null=False)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,27 +1,11 @@
-from django.http import JsonResponse
 from django.shortcuts import render
-import json
 
 from .models import Customer, Order, OrderItem, Product, ShippingAddress
 
 
 def store(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {
-            "get_cart_total|floatformat:2": 0,
-            "get_cart_items": 0,
-            "shipping": False,
-        }
-        cartItems = order["get_cart_items"]
-
     products = Product.objects.all()
-    context = {"products": products, "cartItems": cartItems}
+    context = {"products": products}
     return render(request, "store/store.html", context)
 
 
@@ -30,17 +14,11 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
     else:
         items = []
-        order = {
-            "get_cart_total|floatformat:2": 0,
-            "get_cart_items": 0,
-            "shipping": False,
-        }
-        cartItems = order["get_cart_items"]
+        order = {"get_cart_total": 0, "get_cart_items": 0}
 
-    context = {"items": items, "order": order, "cartItems": cartItems}
+    context = {"items": items, "order": order}
     return render(request, "store/cart.html", context)
 
 
@@ -49,42 +27,9 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
     else:
         items = []
-        order = {
-            "get_cart_total|floatformat:2": 0,
-            "get_cart_items": 0,
-            "shipping": False,
-        }
-        cartItems = order["get_cart_items"]
+        order = {"get_cart_total": 0, "get_cart_items": 0}
 
-    context = {"items": items, "order": order, "cartItems": cartItems}
+    context = {"items": items, "order": order}
     return render(request, "store/checkout.html", context)
-
-
-def updateItem(request):
-    data = json.loads(request.body)
-    productId = data["productId"]
-    action = data["action"]
-
-    print("Action:", action)
-    print("productId:", productId)
-
-    customer = request.user.customer
-    product = Product.objects.get(id=productId)
-    order, created = Order.objects.get_or_create(customer=customer, complete=False)
-
-    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
-
-    if action == "add":
-        orderItem.quantity += 1
-    elif action == "remove":
-        orderItem.quantity -= 1
-
-    orderItem.save()
-
-    if orderItem.quantity <= 0:
-        orderItem.delete()
-
-    return JsonResponse("Item was added", safe=False)
