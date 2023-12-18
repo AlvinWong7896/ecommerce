7ae1a22818698f19691698d091031c90df8dbcc4
CK Wong <alvinwong7896@gmail.com>
2023-12-18 20:08:54 +0800

    Completed Part 3, added checkout form and process order. Bypassed "if total== order.get_cart_total():" as it has generated errors

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+import datetime
 import json
 
 from .models import Customer, Order, OrderItem, Product, ShippingAddress
@@ -75,5 +76,33 @@ def updateItem(request):
 
     if orderItem.quantity <= 0:
         orderItem.delete()
-
     return JsonResponse("Item was added", safe=False)
+
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data["form"]["total"])
+        order.transaction_id = transaction_id
+
+        # if total == order.get_cart_total():
+        order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data["shipping"]["address"],
+                city=data["shipping"]["city"],
+                state=data["shipping"]["state"],
+                zipcode=data["shipping"]["zipcode"],
+            )
+
+    else:
+        print("Usr has not log in..")
+    return JsonResponse("Payment completed!", safe=False)
