2f0fa57d6b6cad49ef8bfd8087c7231147553a45
CK Wong <alvinwong7896@gmail.com>
2023-12-18 12:02:52 +0800

    Include add to Cart function

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -1,29 +1,28 @@
 var updateBtns = document.getElementsByClassName('update-cart')
 
 
-for (i = 0; i < updateBtns.length; i++) {
+for (var i = 0; i < updateBtns.length; i++) {
     updateBtns[i].addEventListener('click', function(){
         var productId = this.dataset.product
         var action = this.dataset.action
         console.log('productId:', productId, 'action:', action)
 
-        console.log('User:', user)
-        if(user === 'AnonymousUser'){
-            console.log('User has not logged in')
+        console.log('USER', user)
+        if (user === 'AnonymousUser') {
+            console.log('Not logged in')
         }else{
             updateUserOrder(productId, action)
         }
-    });
-    
+    })
 }
 
-function updateUserOrder(productId, action){
-    console.log("user is authenticated, sending data..")
+function updateUserOrder(productId, action) {
+    console.log('User is logged in, sending data..')
 
     var url = '/update_item/'
 
     fetch(url, {
-        method: 'POST',
+        method: "POST",
         headers:{
             'Content-Type': 'application/json',
             'X-CSRFToken': csrftoken,
@@ -36,8 +35,7 @@ function updateUserOrder(productId, action){
     })
 
     .then((data) =>{
-        console.log('data', data)
+        console.log('data:', data)
         location.reload()
     })
-
 }
